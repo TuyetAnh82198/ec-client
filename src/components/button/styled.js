4994600715857314d7b1651920ded692ad8bfd4b
TA <tuyetanh82198@gmail.com>
2024-08-26 16:28:52 +0700

    Refactor button

--- src/components/button/styled.js
+++ src/components/button/styled.js
@@ -5,6 +5,7 @@ import { COLOR } from "../../utils/constants";
 
 export const StyledButton = styled(Button)({
   color: "white",
+  padding: "0.5rem 0.75rem",
   backgroundColor: COLOR.MAIN_GREEN,
   "&:hover": {
     backgroundColor: COLOR.DARK_GREEN,
