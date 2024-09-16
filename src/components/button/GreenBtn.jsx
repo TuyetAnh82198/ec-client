d9c659305970086b04c6d3bcffdbb2329e412165
TA <tuyetanh82198@gmail.com>
2024-09-16 19:50:28 +0700

    Updating

--- src/components/button/GreenBtn.jsx
+++ src/components/button/GreenBtn.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 
 import { StyledButton } from "./styled";
 
-const GreenButton = ({ text }) => {
-  return <StyledButton>{text}</StyledButton>;
+const GreenButton = ({ text, type }) => {
+  return <StyledButton type={type || "text"}>{text}</StyledButton>;
 };
 
 GreenButton.propTypes = {
   text: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default GreenButton;
